e25ff3c9c0ec6879efc90807a0631be2024bb03c
MI Worship Server <tech@miiglesiainternacional.org>
2026-09-17 09:18:45 -0600

    NeuralPlay (Mac): tamano minimo de ventana 900x700 (no encoger mas alla)

--- NeuralPlay/Source/Main.cpp
+++ NeuralPlay/Source/Main.cpp
@@ -7587,6 +7587,7 @@ public:
             setFullScreen (true);
            #else
             setResizable (true, true);
+            setResizeLimits (900, 700, 8000, 6000);   // tamaño mínimo: no se encoge más de 900x700
             centreWithSize (getWidth(), getHeight());
            #endif
             setVisible (true);
